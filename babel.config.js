fcbee7846c26ef12ffc5d95cfe3f2a4bfa439e0f
Esi're <esiri92@gmail.com>
2019-09-09 02:37:06 +0100

    Remove console.* statements from release builds

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+const env = process.env.NODE_ENV;
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [env === 'production' && 'transform-remove-console'].filter(Boolean),
 };
