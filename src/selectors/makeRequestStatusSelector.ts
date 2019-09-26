ea90b8e9768a6c30805f5377bdafd91e9a49473b
Esi're <esiri92@gmail.com>
2019-09-26 04:01:41 +0100

    (❤) Idle update... Added requestStatusSelector

--- src/selectors/makeRequestStatusSelector.ts
+++ src/selectors/makeRequestStatusSelector.ts
@@ -3,12 +3,17 @@ import {RequestState} from '@reducers/requestStatusReducer';
 
 export type RequestStatus = [boolean, any];
 
+export const requestStatusSelector = (state: RequestState): RequestStatus => [
+  state.inRequest || false,
+  state.error || null,
+];
+
 export const makeRequestStatusSelector = <S>(
   stateSelector: Selector<S, RequestState>,
 ) => {
   return createSelector<any, RequestState, RequestStatus>(
     stateSelector,
-    state => [state.inRequest || false, state.error || null],
+    requestStatusSelector,
   );
 };
 
