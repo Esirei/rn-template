008ac8bb11439e585533cdc322d426c2d7c36a3c
Esi're <esiri92@gmail.com>
2019-09-27 02:44:24 +0100

    Moved useAuthNavigation to hooks folder.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './store';
 import Navigation from '@navigation/Navigation';
-import {RouteNames, NavigationService} from '@navigation';
-import {userSelector} from '@selectors/sessionSelector';
-
-const useAuthNavigation = () => {
-  // effect only runs once after 1st render
-  useEffect(() => {
-    // modify as you like
-    let oldUser = null; // user before store was updated, change this to have the same structure with initial user object or whatever you want to use to verify user authentication
-    const userHandler = () => {
-      console.log('User handler running...');
-      const user = userSelector(store.getState());
-      if (oldUser !== user && user) {
-        console.log('user logged in, navigate home', user);
-        // should update to: if in auth route(s), navigate to home screen
-        const currentRouteName = NavigationService.currentRouteName();
-        console.log('Navigation currentRoute', currentRouteName);
-        NavigationService.navigate(RouteNames.HOME);
-      } else if (!!oldUser && !user) {
-        console.log('user logged out, navigate to login');
-      }
-      oldUser = user;
-      console.log('User handler ends...');
-    };
-    const storeUnsubscribe = store.subscribe(userHandler);
-    console.log('Store subscribed');
-    return () => {
-      storeUnsubscribe();
-      console.log('Store unsubscribed');
-    };
-  }, []);
-};
+import {NavigationService} from '@navigation';
+import useAuthNavigation from '@hooks/useAuthNavigation';
 
 const App = () => {
   const setNavigator = ref => NavigationService.setRef(ref);
-  // useAuthNavigation();
+  useAuthNavigation();
   return (
     <Provider store={store}>
       <PersistGate loading={<View />} persistor={persistor}>
---
+++ src/hooks/useAuthNavigation.tsx
@@ -0,0 +1,35 @@
+import {useEffect} from 'react';
+import {RouteNames, NavigationService} from '@navigation/index';
+import {userSelector} from '@selectors/sessionSelector';
+import {store} from '../store';
+
+export const useAuthNavigation = () => {
+  // effect only runs once after 1st render
+  useEffect(() => {
+    // modify as you like
+    let oldUser = null; // user before store was updated, change this to have the same structure with initial user object or whatever you want to use to verify user authentication
+    const userHandler = () => {
+      console.log('User handler running...');
+      const user = userSelector(store.getState());
+      if (oldUser !== user && user) {
+        console.log('user logged in, navigate home', user);
+        // should update to: if in auth route(s), navigate to home screen
+        const currentRouteName = NavigationService.currentRouteName();
+        console.log('Navigation currentRoute', currentRouteName);
+        NavigationService.navigate(RouteNames.HOME);
+      } else if (!!oldUser && !user) {
+        console.log('user logged out, navigate to login');
+      }
+      oldUser = user;
+      console.log('User handler ends...');
+    };
+    const storeUnsubscribe = store.subscribe(userHandler);
+    console.log('Store subscribed');
+    return () => {
+      storeUnsubscribe();
+      console.log('Store unsubscribed');
+    };
+  }, []);
+};
+
+export default useAuthNavigation;
