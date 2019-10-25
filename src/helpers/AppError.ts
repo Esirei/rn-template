7ebb81c596380ffdfab1e138d6c4cf71f0a8c6d5
Esi're <esiri92@gmail.com>
2019-10-25 15:29:37 +0100

    Added AppError helper class.

---
+++ src/helpers/AppError.ts
@@ -0,0 +1,41 @@
+class AppError<T> {
+  private errors: Partial<Record<keyof T, string[]>> = {};
+  public message: string = '';
+
+  constructor(e) {
+    if (e) {
+      this.resetWithErrors(e);
+    }
+  }
+
+  setError = (key: keyof T, value: string) => {
+    const array = this.errors[key] ? this.errors[key] : [];
+    array!.push(value); // ! tells typescript value isn't undefined
+    this.errors[key] = array;
+  };
+
+  getError = (key: keyof T): string => {
+    return this.errors[key] ? this.errors[key]!.join('\n') : '';
+  };
+
+  resetError = (key: keyof T) => {
+    this.errors[key] = [];
+  };
+
+  resetWithError = (key: keyof T, value: string) => {
+    this.resetError(key);
+    this.setError(key, value);
+  };
+
+  reset = () => {
+    this.errors = {};
+    this.message = '';
+  };
+
+  resetWithErrors = e => {
+    this.errors = e.errors ? e.errors : {};
+    this.message = e.message ? e.message : '';
+  };
+}
+
+export default AppError;
