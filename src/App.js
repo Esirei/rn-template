b83a1aec077fc24cf0b458a65bec6ffeaefe5060
Esi're <esiri92@gmail.com>
2019-12-02 21:07:21 +0100

    Added react-native-splash-screen dep

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 import { store, persistor } from './store';
 import Navigation from '@navigation/Navigation';
 import { NavigationService } from '@navigation';
@@ -12,6 +13,7 @@ import ApiInterceptors from '@components/ApiInterceptors';
 const App = () => {
   const setNavigator = ref => NavigationService.setRef(ref);
   useAuthNavigation();
+  useEffect(() => SplashScreen.hide(), []);
   return (
     <Provider store={store}>
       <ApiInterceptors />
